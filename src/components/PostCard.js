195633d3b34ca5ea65c831ed3235d8e74d524b76
Stephan <sgriesel@gmail.com>
2020-04-24 12:36:55 +0200

    start with card logic, update posts content

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -1,6 +1,15 @@
 import React from 'react'
+import img from 'gatsby-image'
+import styles from '../css/postcard.module.css'
+import { Link } from 'gatsby'
 
-const PostCard = () => {
+const PostCard = ({ post }) => {
+    const { title, date, author, slug } = post.frontmatter;
+    const img = post.frontmatter.image.childImageSharp.fluid;
+    console.log("<< PostCard: POST >>")
+    console.log(post);
+    console.log("<< Image >>")
+    // console.log(img)
     return (
         <div>
             <h1>Postcard</h1>
