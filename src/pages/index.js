37d3efeda09fc42a80abc2af34679c627e8ece5f
Stephan <sgriesel@gmail.com>
2020-04-24 09:08:22 +0200

    add query & test

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,44 @@
 import React from "react";
 import Layout from '../components/layout';
 import PostList from '../components/PostList';
+import { graphql, useStaticQuery } from 'gatsby';
 
-export default () =>
-    <Layout>
-        <PostList />
-    </Layout>
+const getPosts = graphql`
+{
+    allMdx(sort:{
+      fields:frontmatter___date,
+      order:DESC
+    }) {
+      totalCount
+      edges {
+        node {
+          frontmatter {
+            title
+            slug
+            date(formatString:"D MMMM 'YY" )
+            author
+            image {
+              childImageSharp {
+                fluid {
+                  ...GatsbyImageSharpFluid_withWebp
+                }
+              }
+            }
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
+
+export default () => {
+    const response = useStaticQuery(getPosts);
+    // console.log(response);
+    const posts = response.allMdx.edges
+    return (
+        <Layout>
+            <PostList posts={posts} />
+        </Layout>
+    )
+}
