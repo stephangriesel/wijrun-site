9bf5d93d0bf55f861480eabed17604851b9d05e6
Stephan <sgriesel@gmail.com>
2020-04-24 11:50:55 +0200

    test data & set up css module

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import PostCard from './PostCard';
+import styles from '../css/postlist.module.css';
 
-const PostList = () => {
+const PostList = ({ posts }) => {
+    console.log("<< PostList: POSTS >>")
+    console.log(posts);
     return (
         <div>
             <h1>Postlist</h1>
