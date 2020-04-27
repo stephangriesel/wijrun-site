301dac138ba4c3bfefe163614e57c3384fd395d2
Stephan <sgriesel@gmail.com>
2020-04-27 08:09:36 +0200

    add logo

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import PostCard from './PostCard';
 import styles from '../css/postlist.module.css';
+import logo from '../img/logo.png';
 
 const PostList = ({ posts }) => {
     console.log("<< PostList: POSTS >>")
     console.log(posts);
     return (
         <section className={styles.posts}>
-            <h1>Pieter Piet</h1>
-            <h4>Some text</h4>
+            <div className={styles.header}>
+                <img src={logo} alt="logo" />
+            </div>
             <div className={styles.center}>
                 {posts.map(({ node }, index) => {
                     return <PostCard key={index} post={node} />
