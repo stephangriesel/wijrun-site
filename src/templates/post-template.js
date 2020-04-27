d6a74c2a06575e16fa73e8cf8a92ecd5c91f231c
Stephan <sgriesel@gmail.com>
2020-04-27 06:29:31 +0200

    set up post template structure

---
+++ src/templates/post-template.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from '../css/posttemplate.module.css';
+import { Link, graphql } from 'gatsby';
+import Image from 'gatsby-image';
+import Layout from '../components/layout';
+import { MDXRenderer } from 'gatsby-plugin-mdx';
+
+
+const PostTemplate = () => {
+    return (
+        <div>
+            post template
+        </div>
+    )
+}
+
+export default PostTemplate;
