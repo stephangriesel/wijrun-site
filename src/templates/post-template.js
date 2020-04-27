51580ecd58ebe982cfaab86072ee8044cb2e075a
Stephan <sgriesel@gmail.com>
2020-04-27 07:23:32 +0200

    post logic finish

--- src/templates/post-template.js
+++ src/templates/post-template.js
@@ -6,14 +6,50 @@ import Layout from '../components/layout';
 import { MDXRenderer } from 'gatsby-plugin-mdx';
 
 
-const PostTemplate = ({ pageContext }) => {
-    console.log('<<< PAGE CONTEXT >>>')
-    console.log(pageContext);
+const PostTemplate = ({ data }) => {
+    // console.log('<<< PAGE CONTEXT >>>')
+    // console.log(pageContext);
+    const { title, date, author, image } = data.mdx.frontmatter;
+    const { body } = data.mdx;
+    const img = image.childImageSharp.fluid;
+
     return (
-        <div>
-            post template
-        </div>
+        <Layout>
+            <section className={styles.template}>
+                <Link to='/' className={styles.link}>Back</Link>
+                <div className={styles.info}>
+                    <h1>{title}</h1>
+                    <h4>
+                        <span>by {author}</span> / <span>{date}</span>
+                    </h4>
+                </div>
+                <Image fluid={img} />
+                <div class={styles.content}>
+                    <MDXRenderer>{body}</MDXRenderer>
+                </div>
+            </section>
+        </Layout>
     )
 }
 
+export const query = graphql`
+query getPost($slug:String!){
+    mdx (frontmatter:{slug:{eq:$slug}}){
+      frontmatter {
+        title
+        slug
+        date(formatString:"D MMMM 'YY")
+        author
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid_withWebp
+            }
+          }
+        }
+      }
+      body
+    }
+  }
+`
 export default PostTemplate;
