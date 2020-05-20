e620c4a7344216bf8fd3bf431c9cb66e43187f17
Stephan <sgriesel@gmail.com>
2020-05-20 06:08:23 +0200

    test accordion & props

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,56 +1,17 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { useState } from 'react'
 
-import AccordionSection from './AccordionSection';
-
-class Accordion extends Component {
-    static propTypes = {
-        children: PropTypes.instanceOf(Object).isRequired,
-    };
-
-    constructor(props) {
-        super(props);
-
-        const openSections = {};
-
-        this.state = { openSections };
-    }
-
-    onClick = label => {
-        const {
-            state: { openSections },
-        } = this;
-
-        const isOpen = !!openSections[label];
-
-        this.setState({
-            openSections: {
-                [label]: !isOpen
-            }
-        });
-    };
-
-    render() {
-        const {
-            onClick,
-            props: { children },
-            state: { openSections },
-        } = this;
-
-        return (
-            <div>
-                {children.map(child => (
-                    <AccordionSection
-                        isOpen={!!openSections[child.props.label]}
-                        label={child.props.label}
-                        onClick={onClick}
-                    >
-                        {child.props.children}
-                    </AccordionSection>
-                ))}
+function Accordion(props) {
+    return (
+        <div className="accordion__section">
+            <button className="accordion">
+                <p className="accordion__title">{props.title}</p>
+            </button>
+            <div className="accordion__content">
+                <div className="accordion__text"
+                    dangerouslySetInnerHTML={{ __html: props.content }} />
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 export default Accordion;
