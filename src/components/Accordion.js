e5755f757a02b324e6f90f03fce7b62984381d65
Stephan <sgriesel@gmail.com>
2020-05-23 17:00:18 +0200

    revert, for now

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -8,10 +8,10 @@ function Accordion(props) {
 
     const content = useRef(null);
 
-    const authLink = "https://www.strava.com/oauth/token";
-    var clientId = process.env.client_id;
-    var clientSecret = process.env.client_secret;
-    var refreshToken = process.env.refresh_token;
+    // const authLink = "https://www.strava.com/oauth/token";
+    // var clientId = process.env.client_id;
+    // var clientSecret = process.env.client_secret;
+    // var refreshToken = process.env.refresh_token;
 
     function toggleAccordion() {
         setActiveState(setActive === "" ? "active" : "");
@@ -22,31 +22,31 @@ function Accordion(props) {
         console.log(content.current.scrollHeight);
     }
 
-    function getStravaActivities(res) {
-        console.log(res.access_token)
-        const activitiesLink = `https://www.strava.com/api/v3/athlete/activities?access_token=${res.access_token}`
-        fetch(activitiesLink)
-            .then((res) => console.log(res.json()))
-    }
+    // function getStravaActivities(res) {
+    //     console.log(res.access_token)
+    //     const activitiesLink = `https://www.strava.com/api/v3/athlete/activities?access_token=${res.access_token}`
+    //     fetch(activitiesLink)
+    //         .then((res) => console.log(res.json()))
+    // }
 
-    function reAuthorize() {
-        fetch(authLink, {
-            method: 'post',
-            headers: {
-                'Accept': 'application/json, text/plain, */*',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                client_id: clientId,
-                client_secret: clientSecret,
-                refresh_token: refreshToken,
-                grant_type: 'refresh_token'
-            })
-        }).then(res => res.json())
-            .then(res => getStravaActivities(res))
-    }
+    // function reAuthorize() {
+    //     fetch(authLink, {
+    //         method: 'post',
+    //         headers: {
+    //             'Accept': 'application/json, text/plain, */*',
+    //             'Content-Type': 'application/json'
+    //         },
+    //         body: JSON.stringify({
+    //             client_id: clientId,
+    //             client_secret: clientSecret,
+    //             refresh_token: refreshToken,
+    //             grant_type: 'refresh_token'
+    //         })
+    //     }).then(res => res.json())
+    //         .then(res => getStravaActivities(res))
+    // }
 
-    reAuthorize();
+    // reAuthorize();
 
 
     return (
